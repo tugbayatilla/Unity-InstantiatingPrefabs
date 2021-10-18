7d0f1be59bac6803b86ffda27b80234b87740542
Tugbay Atilla <tatilla@igus.net>
2021-10-18 10:20:32 +0200

    initial commit

---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    public GameObject block;
+    public int width = 10;
+    public int height = 4;
+    public float blockMass = 1.0f;
+	void Start()
+    {
+        for (int y=0; y<height; ++y)
+        {
+            for (int x=0; x<width; ++x)
+            {
+                Vector3 offset = new Vector3(x, y, 0);
+				var rb = block.GetComponent<Rigidbody>();
+				rb.mass = blockMass;
+
+				Instantiate(block, transform.position + offset, Quaternion.identity);
+            }
+        }        
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Unity-InstantiatingPrefabs
+
+- this project and code can be found in unity page.
+- link is https://docs.unity3d.com/Manual/InstantiatingPrefabs.html
